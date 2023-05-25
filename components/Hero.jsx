fcb378f01e4e97e917fa386b79ea2b366a542ba8
dmuhia136 <dmuhia136@gmail.com>
2023-05-25 17:10:35 +0300

    Navbar and hero section added

---
+++ components/Hero.jsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+
+function Hero() {
+  return (
+    <container className="flex flex-row justify-between pt-10 pl-40">
+      {/* left hero */}
+      <div className="flex flex-col space-y-5">
+        <h1 className="text-left text-4xl font-bold ">Here is my portfolio</h1>
+        <p className="w-1/3 font-itallic text-gray-500 text-xl ">
+          I am a fullstack developer working with various technologies to create
+          beautiful websites and mobile applications. Here is a list of my
+          techstack.
+        </p>
+        <div className="grid gap-2 lg:grid-cols-2 sm:grid-cols-1">
+          <div className="">
+            <span className="rounded bg-yellow-300 text-gray-800 p-2 ">1</span>{" "}
+            Flutter for Mobile applications.
+          </div>
+          <div className="mb-4">
+            <span className="rounded bg-yellow-300 text-gray-800 p-2 ">2</span> React
+            js for websites.
+          </div>
+          <div className="mb-4">
+            <span className="rounded bg-yellow-300 text-gray-800 p-2 ">3</span> Next
+            js for websites.
+          </div>
+          <div className="mb-4">
+            <span className="rounded bg-yellow-300 text-gray-800 p-2 ">4</span> Vue js
+            for websites.
+          </div>
+          <div className="mb-4">
+            <span className="rounded bg-yellow-300 text-gray-800 p-2 ">5</span> Node
+            js for backend.
+          </div>
+          <div className="mb-4">
+            <span className="rounded bg-yellow-300 text-gray-800 p-2 ">6</span> Nest
+            js for backend.
+          </div>
+          <div className="mb-4">
+            <span className="rounded bg-yellow-300 text-gray-800 p-2 ">7</span> Golang
+            for backend.
+          </div>
+        </div>
+      </div>
+      {/* right hero */}
+      <div><Image
+      src="/development.png"
+      width={600}
+      height={600}
+      className='rotate-10 ml-4'
+      alt="Dennis Njoroge Muhia"
+    /></div>
+    </container>
+  );
+}
+
+export default Hero;
+ 
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import Image from 'next/image';
+ 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='flex flex-row justify-between pl-10 pr-10 p-2 place-items-center'>
+      <div className='text-3xl font-bold text-center flex gap-3 place-items-center'>
+      <Image
+      src="/dennis.png"
+      width={50}
+      height={50}
+      className='rounded-full'
+      alt="Dennis Njoroge Muhia"
+    />
+      Dennis Muhia</div>
+      
+      <a href='https://github.com/dmuhia136' className='flex place-items-center pr-2 rounded-full bg-gray-800 text-white hover:bg-gray-500 cursor-pointer p-1 text-md font-normal '>
+      <Image
+      src="/github.png"
+      width={20}
+      height={20}
+      className='rounded-full'
+      alt="Dennis Njoroge Muhia"
+    />
+      Github</a>
+    </nav>
   )
 }
 
