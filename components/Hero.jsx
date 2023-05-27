9f4db8e15e723f8d67c38e508f7d7d9af8e63c3c
dmuhia136 <dmuhia136@gmail.com>
2023-05-27 18:48:15 +0300

    flex box

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,13 +14,13 @@ function Hero() {
           beautiful websites and mobile applications. Here is a list of my
           techstack.
         </p>
-        <div className="md:flex md:gap-4 grid md:grid-cols-2 grid-cols-1 space-y-2">
+        <div className="md:grid md:grid-cols-2 lg:flex md:space-x-2 md:space-y-2 lg:space-x-2">
           <Accomplish title="02" desc="Awards" />
           <Accomplish title="20+" desc="Clients" />
           <Accomplish title="5" desc="Years of experience" />
           <Accomplish title="70+" desc="Projects" />
         </div>
-        <div className="grid grid-cols-1 space-y-2  lg:grid-cols-2">
+        <div className="grid grid-cols-1 gap-2  lg:grid-cols-2">
           <div className="">
             <span className="rounded bg-yellow-300 text-gray-800 p-2 ">1</span>{" "}
             Flutter for Mobile applications.
